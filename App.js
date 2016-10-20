63db8ef4b2583f9b56454f7f6020bda79e639d6a
krzysieki <krzysieki@users.noreply.github.com>
2016-10-20 17:11:05 +0200

    Testing properties and default values

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello</h1>;
+    let txt = this.props.txt;
+    return (
+      <div>
+        <h1>Title</h1>
+        <b>{txt}</b>
+      </div>
+    );
   }
 }
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: 'this is the default text'
+};
+
+ReactDOM.render(
+  <App cat={5} />,
+  document.getElementById('app')
+);
 
 export default App;
